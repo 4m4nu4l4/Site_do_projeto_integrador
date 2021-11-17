a0b370c7d5d9ddb0e9b503a1976057940f81744a
4m4nu4l4 <65728939+4m4nu4l4@users.noreply.github.com>
2021-11-17 15:40:34 -0300

    17/11
    mudamos o fundinho uhuuulll

--- novo_html/teste_novo.php
+++ novo_html/teste_novo.php
@@ -15,7 +15,7 @@
 
 <body>
   
- -S localhost:8000
+ 
   <nav class="navbar navbar-expand-lg navbar-light bg-info" style="background-color:#5d7ead !important">
     <img src="imagens/foto sem fundo.png" width="150" height="100" alt="">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#textoNavbar"
